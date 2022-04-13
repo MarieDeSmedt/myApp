bdf589d28ccc7b481967454492b1b778d364e99d
mari <desmedtmarie@outlook.com>
2022-04-13 19:47:50 +0200

    update

--- map_app/main.py
+++ map_app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, redirect, session
+from flask import Flask, render_template, json, request, redirect, session, flash
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 import sys
@@ -30,46 +30,50 @@ def showSignUp():
 
 @app.route('/api/signup', methods=['POST'])
 def signUp():
-    try:
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
+    
+        try:
+            _name = request.form['inputName']
+            _email = request.form['inputEmail']
+            _password = request.form['inputPassword']
 
-        # validate the received values
-        if _name and _email and _password:
+            # validate the received values
+            if _name and _email and _password:
 
             # All Good, let's call MySQL
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
-            data = cursor.fetchall()
+                conn = mysql.connect()
+                cursor = conn.cursor()
+                _hashed_password = generate_password_hash(_password)
+                cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+                data = cursor.fetchall()
 
-            if len(data) == 0:
-                conn.commit()
-                return json.dumps({'message': 'User created successfully !'})
+                if len(data) == 0:
+                    conn.commit()
+                    flash('success')
+                    return json.dumps({'message': 'User created successfully !'})
+                else:
+                    return json.dumps({'error': str(data[0])})
             else:
-                return json.dumps({'error': str(data[0])})
-        else:
-            return json.dumps({'html': '<span>Enter the required fields</span>'})
-
-    except Exception as e:
-        return json.dumps({'error': str(e)})
-    finally:
-        cursor.close()
-        conn.close()
+                return json.dumps({'html': '<span>Enter the required fields</span>'})
+   
+        except Exception as e:
+            return json.dumps({'error': str(e)})
+        finally:
+            cursor.close()
+            conn.close()
 
 @app.route('/showSignin')
 def showSignin():
     return render_template('signin.html')
 
+
 @app.route('/userHome')
 def userHome():
     if session.get('user'):
-        return render_template('userHome.html')
+        return render_template('userhome.html')
     else:
         return render_template('error.html',error = 'Unauthorized Access')
+
 
 @app.route('/validateLogin',methods=['POST'])
 def validateLogin():
@@ -82,14 +86,13 @@ def validateLogin():
  
         con = mysql.connect()
         cursor = con.cursor()
-        cursor.callproc('sp_validateLogin',(_username))
+        cursor.callproc('sp_validateLogin',(_username,))
         data = cursor.fetchall()
  
         if len(data) > 0:
            
             if check_password_hash(str(data[0][3]),_password):
                 session['user'] = data[0][0]
-                
                 return redirect('/userHome')
             else:
                
@@ -100,7 +103,7 @@ def validateLogin():
  
  
     except Exception as e:
-        return render_template('error.html',error = _username)
+        return render_template('error.html',error = str(e))
     finally:
         cursor.close()
         con.close()
@@ -112,4 +115,5 @@ def validateLogin():
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
     app.run(debug=True)
